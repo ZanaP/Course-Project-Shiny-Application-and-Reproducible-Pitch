f2b701645519d4736afdf41217bcb9c8ef4dcff3
ZanaP <zana_pekmez@hotmail.com>
2018-10-04 21:43:50 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,70 +1,24 @@
 library(shiny)
+data(USArrests)
 
-# Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
+# Define UI for application that draws a histogram
+ui <- fluidPage(
   
-  # Application title
-  headerPanel("Miles Per Gallon"),
-  
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("Cylinders" = "cyl", 
-                     "Transmission" = "am", 
-                     "Gears" = "gear")),
-    
-    checkboxInput("outliers", "Show outliers", FALSE)
-  ),
-  
-  mainPanel()
-))
-mpgData <- mtcars
-mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+  titlePanel("Murder rates per diverse states in USA"),
+  sliderInput(inputId = "num", label = "Choose a number",
+              value = 150, min = 20,max = 530),
+  plotOutput("hist")
+)
 
-# Define server logic required to plot various variables against mpg
-shinyServer(function(input, output) {
+server <- function(input, output) {
   
-  # Compute the forumla text in a reactive expression since it is 
-  # shared by the output$caption and output$mpgPlot expressions
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
+  output$hist <- renderPlot({
+    data("USArrests")
+    x <- USArrests$Murder
+    hist(rnorm(input$num))
+    hist(col = 'darkgray', border = 'white')
   })
-  
-  # Return the formula text for printing as a caption
-  output$caption <- renderText({
-    formulaText()
-  })
-  
-  # Generate a plot of the requested variable against mpg and only 
-  # include outliers if requested
-  output$mpgPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
-})
-# Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Miles Per Gallon"),
-  
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("Cylinders" = "cyl", 
-                     "Transmission" = "am", 
-                     "Gears" = "gear")),
-    
-    checkboxInput("outliers", "Show outliers", FALSE)
-  ),
-  
-  # Show the caption and plot of the requested variable against mpg
-  mainPanel(
-    h3(textOutput("caption")),
-    
-    plotOutput("mpgPlot")
-  )
-))
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
