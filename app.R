7d4ffebd9ec8c4624497cf80bad4c73b4d1b03bb
ZanaP <zana_pekmez@hotmail.com>
2018-10-04 21:40:18 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,51 +1,70 @@
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-
 library(shiny)
-library(e1071)
 
-data(Titanic)
-df <- as.data.frame(Titanic)
-mod <- naiveBayes(Survived ~ ., data = Titanic)
-
-shinyUI( pageWithSidebar(
-  headerPanel( "Naive Bayes Classifier applied to Titanic dataset"),
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Miles Per Gallon"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
   sidebarPanel(
-    img(src="titanic.jpg"),
-    h4("Titanic passenger's attributes:"),
-    br(),
-    selectInput( "cl", "Class", levels(df[1,1]), "Crew"),
-    selectInput( "se", "Sex", levels(df[1,2]), "Male"),
-    selectInput( "ag", "Age", levels(df[1,3]), "Adult"),
-    br(),
-    h4("Naive Bayes associated prediction:"),
-    br(),
-    h5("Survived ?"),
-    textOutput("result")
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear")),
+    
+    checkboxInput("outliers", "Show outliers", FALSE)
   ),
+  
+  mainPanel()
+))
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+# Define server logic required to plot various variables against mpg
+shinyServer(function(input, output) {
+  
+  # Compute the forumla text in a reactive expression since it is 
+  # shared by the output$caption and output$mpgPlot expressions
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg and only 
+  # include outliers if requested
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+})
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Miles Per Gallon"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear")),
+    
+    checkboxInput("outliers", "Show outliers", FALSE)
+  ),
+  
+  # Show the caption and plot of the requested variable against mpg
   mainPanel(
-    h3("Titanic data set"),
-    p("The Titanic data set provides information on the fate of 2.201 passengers on the fatal maiden voyage of the ocean liner, summarized according to economic status (class), sex, age and survival."),
-    h3("Naive Bayes prediction"),
-    p("Based on this data set this ShinyApp uses a Naive Bayes classifier prediction model and display the associated stats numbers based on passenger's attributes"),
-    plotOutput('plot1')
+    h3(textOutput("caption")),
+    
+    plotOutput("mpgPlot")
   )
 ))
-shinyServer  
-  function(input, output, session) 
-  
-  output$plot1 <- renderPlot({
-    
-    selectedData <- df[df$Class==input$cl & df$Sex==input$se & df$Age==input$ag,5]
-    bplt <- barplot(selectedData,
-                    beside=TRUE, horiz=TRUE, xlim=c(0,700),
-                    main="Titanic stats based on selected passengers's attributes",
-                    ylab="Total",
-                    col=c("black", "grey"),
-                    legend = c("Deads", "Survivors")
-    )
-    text(x=selectedData+20,
-         y=bplt,
-         labels=as.character(selectedData),
-         xpd=TRUE)
-  })
